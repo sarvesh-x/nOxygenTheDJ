34cdf7130b1fcf925b922867410468ccd285b47c
ArreyShankar <68198811+arreyshankar@users.noreply.github.com>
2021-10-24 11:00:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/**
- * Module Imports
- */
+
 const { Client, Collection } = require("discord.js");
 const { readdirSync } = require("fs");
 const { join } = require("path");
@@ -45,9 +43,7 @@ i18n.configure({
   }
 });
 
-/**
- * Client Events
- */
+
 client.on("ready", () => {
   console.log(`${client.user.username} ready!`);
   client.user.setActivity(`Some Mood Booster`, { type: "LISTENING" });
@@ -55,9 +51,7 @@ client.on("ready", () => {
 client.on("warn", (info) => console.log(info));
 client.on("error", console.error);
 
-/**
- * Import all commands
- */
+
 const commandFiles = readdirSync(join(__dirname, "commands")).filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
   const command = require(join(__dirname, "commands", `${file}`));
